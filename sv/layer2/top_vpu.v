5602e6220c162f38d5f8964f5a60f476e7ba1b63
slim msehli <dmsehlislim@gmail.com>
2026-02-16 18:45:57 +0100

    npu layer update for 3 layer setup

--- sv/layer2/top_vpu.v
+++ sv/layer2/top_vpu.v
@@ -9,10 +9,11 @@ module top();
     parameter FRAC_BITS = 8;
     parameter CLK_PERIOD = 10;
     parameter LENGTH = 9;
+    parameter LAYERS = 3;
     
     reg clk;
     reg rst;
-    reg start1, start2, start3;
+    reg [LAYERS-1:0] start;
     
     // Input matrices
     reg [DATA_WIDTH-1:0] matrix_a [0:8];
@@ -24,22 +25,18 @@ module top();
     reg [DATA_WIDTH-1:0] mat_weights_2 [0:8];
     
     // VPU control
-    reg vpu_enable;
-    reg [2:0] vpu_operation;
-    reg [ACC_WIDTH-1:0] bias_value;
-    reg [ACC_WIDTH-1:0] scale_factor;
+    reg [LAYERS-1:0] vpu_enable ;
+    reg [2:0] vpu_operation [0:LAYERS-1];
+    reg [ACC_WIDTH-1:0] bias_value [0:LAYERS-1];
+    reg [ACC_WIDTH-1:0] scale_factor [0:LAYERS-1];
     
     // Outputs
-    wire [ACC_WIDTH-1:0] raw_result1 [0:LENGTH-1];
-    wire [ACC_WIDTH-1:0] raw_result2 [0:LENGTH-1];
-    wire [ACC_WIDTH-1:0] raw_result3 [0:LENGTH-1];
-  
-    wire [ACC_WIDTH-1:0] result1 [0:LENGTH-1];
-    wire [ACC_WIDTH-1:0] result2 [0:LENGTH-1];
-    wire [ACC_WIDTH-1:0] result3 [0:LENGTH-1];
+    wire [ACC_WIDTH-1:0] raw_result [0:LAYERS-1][0:LENGTH-1];
+  	
+  	wire [ACC_WIDTH-1:0] result [0:LAYERS-1][0:LENGTH-1];
     
-    wire controller_done1, controller_done2, controller_done3;
-    wire vpu_valid;
+    wire [LAYERS-1:0] controller_done;
+    wire [LAYERS-1:0] vpu_valid;
     
     // VPU operation codes
     localparam OP_PASSTHROUGH = 3'd0;
@@ -74,17 +71,17 @@ module top();
     ) dut1 (
         .clk(clk),
         .rst(rst),
-        .start(start1),
+        .start(start[0]),
         .matrix_a(matrix_a),
         .matrix_b(matrix_b),
-        .vpu_enable(vpu_enable),
-        .vpu_operation(vpu_operation),
-        .bias_value(bias_value),
-        .scale_factor(scale_factor),
-        .raw_result(raw_result1),
-        .result(result1),
-        .controller_done(controller_done1),
-        .vpu_valid(vpu_valid)
+        .vpu_enable(vpu_enable[0]),
+        .vpu_operation(vpu_operation[0]),
+        .bias_value(bias_value[0]),
+        .scale_factor(scale_factor[0]),
+        .raw_result(raw_result[0]),
+        .result(result[0]),
+        .controller_done(controller_done[0]),
+        .vpu_valid(vpu_valid[0])
     );
     
     // Instantiate DUT
@@ -96,17 +93,17 @@ module top();
     ) dut2 (
         .clk(clk),
         .rst(rst),
-        .start(start2),
+        .start(start[1]),
         .matrix_a(matrix_a),
         .matrix_b(matrix_b),
-        .vpu_enable(vpu_enable),
-        .vpu_operation(vpu_operation),
-        .bias_value(bias_value),
-        .scale_factor(scale_factor),
-        .raw_result(raw_result2),
-        .result(result2),
-        .controller_done(controller_done2),
-        .vpu_valid(vpu_valid)
+        .vpu_enable(vpu_enable[1]),
+        .vpu_operation(vpu_operation[1]),
+        .bias_value(bias_value[1]),
+        .scale_factor(scale_factor[1]),
+        .raw_result(raw_result[1]),
+        .result(result[1]),
+        .controller_done(controller_done[1]),
+        .vpu_valid(vpu_valid[1])
     );
     
     // Instantiate DUT
@@ -118,17 +115,17 @@ module top();
     ) dut3 (
         .clk(clk),
         .rst(rst),
-        .start(start3),
+        .start(start[2]),
         .matrix_a(matrix_a),
         .matrix_b(matrix_b),
-        .vpu_enable(vpu_enable),
-        .vpu_operation(vpu_operation),
-        .bias_value(bias_value),
-        .scale_factor(scale_factor),
-        .raw_result(raw_result3),
-        .result(result3),
-        .controller_done(controller_done3),
-        .vpu_valid(vpu_valid)
+        .vpu_enable(vpu_enable[2]),
+        .vpu_operation(vpu_operation[2]),
+        .bias_value(bias_value[2]),
+        .scale_factor(scale_factor[2]),
+        .raw_result(raw_result[2]),
+        .result(result[2]),
+        .controller_done(controller_done[2]),
+        .vpu_valid(vpu_valid[2])
     );
     
     integer fd;
@@ -140,16 +137,17 @@ module top();
         
         // Initialize
         rst = 1;
-        start1 = 0;start2 = 0;start3 = 0;
-        vpu_enable = 0;
-        vpu_operation = OP_PASSTHROUGH;
-        bias_value = 0;
-        scale_factor = 0;
+        start = 3'b000;
+        vpu_enable = 3'b000;
+        bias_value = {0, 0, 0};
+        scale_factor = {0, 0, 0};
+        vpu_operation = {OP_RELU, OP_RELU, OP_RELU};
         
         // Reset
         #(CLK_PERIOD*2);
         rst = 0;
         #(CLK_PERIOD);
+        
         
         //###########################################
         // first layer
@@ -169,28 +167,27 @@ module top();
         end
         
         // Start systolic array computation
-        vpu_operation = OP_RELU;
-        vpu_enable = 1;
-        start1 = 1;
+        vpu_enable = 3'b001;
+        start = 3'b001;
         #(CLK_PERIOD);
-        vpu_enable = 0;
+        vpu_enable = 3'b000;
         
         
         // Wait for completion
-        wait(controller_done1);
+        wait(controller_done[0]);
         #(CLK_PERIOD*5);
-        start1 = 0;
+        start = 3'b000;
         
         // Display raw results
         $display("\n--- Layer 1 Raw Systolic Array Output (C = A × B) ---");
         for (integer i=1; i<LENGTH+1;i++) begin
-        	$write(" %0d ", raw_result1[i-1]);
+        	$write(" %0d ", raw_result[0][i-1]);
         	if (i%3==0) $write("\n");
         end
         
         //###########################################
         // second layer
-        matrix_a = raw_result1;
+        matrix_a = raw_result[0];
         matrix_b = mat_weights_1;
         $display("\n--- Layer 2 ");
         $display("Matrix A:");
@@ -206,28 +203,27 @@ module top();
         end
         
         // Start systolic array computation
-        vpu_operation = OP_RELU;
-        vpu_enable = 1;
-        start2 = 1;
+        vpu_enable = 3'b010;
+        start = 3'b010;
         #(CLK_PERIOD);
-        vpu_enable = 0;
+        vpu_enable = 3'b000;
         
         
         // Wait for completion
-        wait(controller_done2);
+        wait(controller_done[1]);
         #(CLK_PERIOD*5);
-        start2 = 0;
+        start = 3'b000;
         
         // Display raw results
         $display("\n--- Layer 2 Raw Systolic Array Output (C = A × B) ---");
         for (integer i=1; i<LENGTH+1;i++) begin
-        	$write(" %0d ", raw_result2[i-1]);
+        	$write(" %0d ", raw_result[1][i-1]);
         	if (i%3==0) $write("\n");
         end
         
         //###########################################
         // Third layer
-        matrix_a = raw_result2;
+        matrix_a = raw_result[1];
         matrix_b = mat_weights_2;
         $display("\n--- Layer 3 ");
         $display("Matrix A:");
@@ -243,22 +239,21 @@ module top();
         end
         
         // Start systolic array computation
-        vpu_operation = OP_RELU;
-        vpu_enable = 1;
-        start3 = 1;
+        vpu_enable = 3'b100;
+        start = 3'b100;
         #(CLK_PERIOD);
-        vpu_enable = 0;
+        vpu_enable = 3'b000;
         
         
         // Wait for completion
-        wait(controller_done3);
+        wait(controller_done[2]);
         #(CLK_PERIOD*5);
-        start3 = 0;
+        start = 3'b000;
         
         // Display raw results
         $display("\n--- Layer 3 Raw Systolic Array Output (C = A × B) ---");
         for (integer i=1; i<LENGTH+1;i++) begin
-        	$write(" %0d ", raw_result3[i-1]);
+        	$write(" %0d ", raw_result[2][i-1]);
         	if (i%3==0) $write("\n");
         end
         
@@ -277,13 +272,13 @@ module top();
         // Display raw results
         $display("\n\n--- Output Matrix ---\n");
         for (integer i=1; i<LENGTH+1;i++) begin
-        	$write(" %0d ", raw_result3[i-1]);
+        	$write(" %0d ", raw_result[2][i-1]);
         	if (i%3==0) $write("\n");
         end
         
         fd = $fopen("../memories/npu_output.hex", "w");
 				for (integer i = 0; i < LENGTH; i++) begin
-					$fdisplay(fd, "%01h", raw_result3[i]);
+					$fdisplay(fd, "%01h", raw_result[2][i]);
 				end
 				$fclose(fd);        
         $finish;
